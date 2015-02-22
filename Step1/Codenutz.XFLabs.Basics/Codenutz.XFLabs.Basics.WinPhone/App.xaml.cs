0917ca4626915b8f4adebb686b0d01039dd4ffda
Matt <matthew.whetton@yahoo.co.uk>
2015-02-22 16:16:27 +0000

    Lots of renaming ahnnd moving

--- Step1/Codenutz.XFLabs.Basics/Codenutz.XFLabs.Basics.WinPhone/App.xaml.cs
+++ Step1/Codenutz.XFLabs.Basics/Codenutz.XFLabs.Basics.WinPhone/App.xaml.cs
@@ -12,46 +12,22 @@ namespace Codenutz.XFLabs.Basics.WinPhone
 {
 	public partial class App : Application
 	{
-		/// <summary>
-		/// Provides easy access to the root frame of the Phone Application.
-		/// </summary>
-		/// <returns>The root frame of the Phone Application.</returns>
 		public static PhoneApplicationFrame RootFrame { get; private set; }
 
-		/// <summary>
-		/// Constructor for the Application object.
-		/// </summary>
 		public App()
 		{
-			// Global handler for uncaught exceptions.
 			UnhandledException += Application_UnhandledException;
-
-			// Standard XAML initialization
+			
 			InitializeComponent();
 
-			// Phone-specific initialization
 			InitializePhoneApplication();
 
-			// Language display initialization
 			InitializeLanguage();
 
-			// Show graphics profiling information while debugging.
 			if (Debugger.IsAttached)
 			{
-				// Display the current frame rate counters.
 				Application.Current.Host.Settings.EnableFrameRateCounter = true;
 
-				// Show the areas of the app that are being redrawn in each frame.
-				//Application.Current.Host.Settings.EnableRedrawRegions = true;
-
-				// Enable non-production analysis visualization mode,
-				// which shows areas of a page that are handed off to GPU with a colored overlay.
-				//Application.Current.Host.Settings.EnableCacheVisualization = true;
-
-				// Prevent the screen from turning off while under the debugger by disabling
-				// the application's idle detection.
-				// Caution:- Use this under debug mode only. Application that disables user idle detection will continue to run
-				// and consume battery power when the user is not using the phone.
 				PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
 			}
 
--- Step1/Codenutz.XFLabs.Basics/Codenutz.XFLabs.Basics.iOS/AppDelegate.cs
+++ Step1/Codenutz.XFLabs.Basics/Codenutz.XFLabs.Basics.iOS/AppDelegate.cs
@@ -10,13 +10,6 @@ namespace Codenutz.XFLabs.Basics.iOS
 	[Register("AppDelegate")]
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this 
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init();
